ee0e752261e8ce7d26e31a29b64f2858aa1c23b6
吴庆锋 <wuqingfeng3@newhope.cn>
2021-12-01 11:17:58 +0800

    字符串匹配/测试用例

--- src/test/java/com/git/wuqf/algorithm/stringmatch/StringMatchTest.java
+++ src/test/java/com/git/wuqf/algorithm/stringmatch/StringMatchTest.java
@@ -3,8 +3,10 @@ package com.git.wuqf.algorithm.stringmatch;
 import org.junit.jupiter.api.Test;
 
 public class StringMatchTest {
-    String source ="abcdefg";
-    String pattern="ef";
+//    String source ="abcdefg";
+//    String pattern="ef";
+    String source ="BBC ABCDAB ABCDABCDABDE";
+    String pattern="ABCDABD";
     @Test
     public void testBruteForce() {
         BruteForce b=new BruteForce();
