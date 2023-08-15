faef074d6397e4879b850aaccd7acd40f8babbb8
wuqingfeng <wuqingfeng234@163.com>
2023-08-15 21:46:35 +0800

    简化代码&优化代码时间消耗

--- src/main/java/com/git/wuqf/algorithm/sort/merge/MergeSort.java
+++ src/main/java/com/git/wuqf/algorithm/sort/merge/MergeSort.java
@@ -6,36 +6,45 @@ import com.git.wuqf.algorithm.sort.Sort;
  * Created by wuqf on 2020/5/14.
  */
 public class MergeSort implements Sort {
-    int[] result;
 
+    int[] help;
     @Override
     public int[] sort(int[] arr) {
-        int len = arr.length;
-        result = new int[len];
-        recursiveMergeSort(arr, 0, len - 1);
-        return result;
+        help= new int[arr.length];
+        mergesort(arr, 0, arr.length - 1);
+        return arr;
     }
 
     //recursive
-    public void recursiveMergeSort(int[] arr, int start, int end) {
+    public void mergesort(int[] arr, int start, int end) {
         if (start >= end) {
             return;
         }
-        int len = end - start, mid = start + (len >> 1);
-        int lstart = start, lend = mid;
-        int rstart = mid + 1, rend = end;
-        recursiveMergeSort(arr, lstart, lend);
-        recursiveMergeSort(arr, rstart, rend);
+        int length = end - start ;
+        int mid = start + (length >> 1);
+        mergesort(arr, start, mid);
+        mergesort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+
+    public void merge(int[] arr, int start, int mid, int end) {
+        int lstart = start;
+        int rstart = mid + 1;
+
         int k = start;
-        while (lstart <= lend && rstart <= rend) {
-            result[k++] = arr[lstart] < arr[rstart] ? arr[lstart++] : arr[rstart++];
+        while (lstart <= mid && rstart <= end) {
+            help[k++] = arr[lstart] < arr[rstart] ? arr[lstart++] : arr[rstart++];
         }
-        while (lstart <= lend) {
-            result[k++] = arr[lstart++];
+        while (lstart <= mid) {
+            help[k++] = arr[lstart++];
         }
-        while (rstart <= rend) {
-            result[k++] = arr[rstart++];
+        while (rstart <= end) {
+            help[k++] = arr[rstart++];
         }
+        for (int i = start; i <= end; i++) {
+            arr[i] = help[i];
+        }
+        System.out.printf("xx");
     }
 
     //iteration
